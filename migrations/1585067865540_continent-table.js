c3be04d1abb23a32d211331a863de1bccd43c06c
COVID-GUH <guh1337@protonmail.ch>
2020-03-24 12:47:56 -0400

    continent

--- migrations/1585067865540_continent-table.js
+++ migrations/1585067865540_continent-table.js
@@ -1,3 +1,21 @@
-exports.up = (pgm) => {};
+exports.up = (pgm) => {
+  pgm.createTable("continent", {
+    continent_id: "id",
+    name: {
+      type: "VARCHAR(32)",
+      notNull: true
+    },
+    iso_code: {
+      type: "VARCHAR(2)",
+      notNull: true
+    }
+  });
+  pgm.createIndex("continent", ["name"], { unique: true });
+  pgm.createIndex("continent", ["iso_code"], { unique: true });
+};
 
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+  pgm.dropIndex("continent", ["name"], { unique: true });
+  pgm.dropIndex("continent", ["iso_code"], { unique: true });
+  pgm.dropTable("contient");
+};
